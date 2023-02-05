3e91b28c20105688a180c4377e599ac675f51894
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-05 10:02:44 +0530

    day-1

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Kutubkhan Makda</Text>
+      <Text>Maleka</Text>
       <StatusBar style="auto" />
     </View>
   );
