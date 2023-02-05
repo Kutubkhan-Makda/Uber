487bc668b27d83b5ce0557ab3f3e1ab12f4509e1
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-05 12:32:32 +0530

    day-1

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import react from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {Provider} from 'react-redux'
+import { store } from './store';
 
 export default function App() {
   return (
-    <Provider store={}>
+    <Provider store={store}>
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Kutubkhan Makda</Text>
