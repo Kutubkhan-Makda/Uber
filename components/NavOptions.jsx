5a95a741e5516f7ede627788778612f55dfa428a
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-07 19:12:09 +0530

    day-2

--- components/NavOptions.jsx
+++ components/NavOptions.jsx
@@ -1,6 +1,21 @@
 import { Text, View } from 'react-native'
 import React from 'react'
 
+const data = [
+  {
+    id:'123',
+    title:'Get a Ride',
+    image:'https://links.papareact.com/3pn',
+    screen:'MapScreen',
+  },
+  {
+    id:'456',
+    title:'Order Food',
+    image:'https://links.papareact.com/28w',
+    screen:'EatsScreen',
+  },
+];
+
 const NavOptions = () => {
   return (
     <View>
