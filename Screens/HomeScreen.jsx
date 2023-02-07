1442fd50bac179fbcf53db37dc246c8de8d36990
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-07 17:07:08 +0530

    day-2

--- Screens/HomeScreen.jsx
+++ Screens/HomeScreen.jsx
@@ -1,11 +1,13 @@
 import { Image, Text, View } from 'react-native'
 import React from 'react'
+import NavOptions from '../components/NavOptions'
 
 const HomeScreen = () => {
   return (
     <View className='bg-white h-full'>
       <View className='p-5'>
         <Image source={{uri:'https://links.papareact.com/gzs'}} style={{width:100,height:100,resizeMode:'contain'}}/>
+        <NavOptions/>
       </View>
     </View>
   )
--- components/NavOptions.jsx
+++ components/NavOptions.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 import React from 'react'
 
 const NavOptions = () => {
@@ -10,5 +10,3 @@ const NavOptions = () => {
 }
 
 export default NavOptions
-
-const styles = StyleSheet.create({})
