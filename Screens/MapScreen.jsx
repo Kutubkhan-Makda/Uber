a80a13ebe9cbaaf5ff28bbbc6d2a10e407a27e68
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-13 16:20:40 +0530

    day-7

--- Screens/MapScreen.jsx
+++ Screens/MapScreen.jsx
@@ -5,7 +5,6 @@ import Map from '../components/Map'
 const MapScreen = () => {
   return (
     <View>
-      <Text>MapScreen</Text>
       <View className='h-1/2'>
         <Map/>
       </View>
--- components/Map.jsx
+++ components/Map.jsx
@@ -1,11 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import MapView from 'react-native-maps'
 
 const Map = () => {
   return (
-    <View>
-      <Text>Map</Text>
-    </View>
+    <MapView className='flex-1' initialRegion={{latitude:37.78825,longitude:-122.4324,latitudeDelta:0.0922,longitudeDelta:0.0421,}}/>
   )
 }
 
