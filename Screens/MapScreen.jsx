7c3dce0015cb975e100c3c190b375b50f29b58e9
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-13 16:09:31 +0530

    day-7

--- Screens/MapScreen.jsx
+++ Screens/MapScreen.jsx
@@ -1,11 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Map from '../components/Map'
 
 const MapScreen = () => {
   return (
     <View>
       <Text>MapScreen</Text>
-      <View className='h-1/2'></View>
+      <View className='h-1/2'>
+        <Map/>
+      </View>
       <View className='h-1/2'></View>
     </View>
   )
