7cab8d8f71b594f931c09d40ab3d0c4492bcdf39
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-13 18:21:34 +0530

    day-7

--- components/Map.jsx
+++ components/Map.jsx
@@ -2,16 +2,28 @@ import { StyleSheet } from 'react-native'
 import React from 'react'
 import MapView,{Marker} from 'react-native-maps'
 import { useSelector } from 'react-redux'
-import { selectOrigin } from '../slices/navSlice'
+import { selectDestination, selectOrigin } from '../slices/navSlice'
+import MapViewDirections from 'react-native-maps-directions'
+import { GOOGLE_MAPS_KEY } from '@env'
 
 const Map = () => {
-  const origin = useSelector(selectOrigin)
+  const origin = useSelector(selectOrigin);
+  const destination = useSelector(selectDestination);
 
   return (
     <MapView 
     className='flex-1' 
     mapType='satellite' 
     initialRegion={{latitude:37.78825,longitude:-122.4324,latitudeDelta:0.0722,longitudeDelta:0.0321,}}>
+      {origin && destination && (
+        <MapViewDirections
+        origin={origin.description}
+        destination={destination.description}
+        apikey={GOOGLE_MAPS_KEY}
+        strokeColor='black'
+        strokeWidth={3}
+        />
+      )}
       {origin?.location && (
         <Marker coordinate={{
           latitude:37.78825,
