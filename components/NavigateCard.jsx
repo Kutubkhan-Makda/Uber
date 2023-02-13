dc83c441709f424d9a5768d55766090edbe64cb5
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-13 18:12:28 +0530

    day-7

--- components/NavigateCard.jsx
+++ components/NavigateCard.jsx
@@ -2,8 +2,14 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
 import { GOOGLE_MAPS_KEY } from '@env'
+import { useDispatch } from 'react-redux'
+import { setDestination } from '../slices/navSlice'
+import { useNavigation } from '@react-navigation/native'
 
 const NavigateCard = () => {
+    const dispatch = useDispatch();
+    const navigation = useNavigation();
+
   return (
     <View className='bg-white flex-1'>
       <Text className='text-center py-5 text-xl'>NavigateCard</Text>
@@ -15,6 +21,10 @@ const NavigateCard = () => {
             query={{key:GOOGLE_MAPS_KEY,language:'en'}} 
             fetchDetails={true}
             enablePoweredByContainer={false}
+            onPress={(data,details=null)=>{
+                dispatch(setDestination({location:details.geometry.location,description:data.description}));
+                navigation.navigate('RideOptionCard')
+            }}
             nearbyPlacesAPI='GooglePlacesSearch' 
             debounce={400}/>
         </View>
