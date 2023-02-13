83c2102fae755ebbbaad47bebc622a9ee8dbec35
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-13 17:33:47 +0530

    day-7

--- components/NavigateCard.jsx
+++ components/NavigateCard.jsx
@@ -1,10 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
 
 const NavigateCard = () => {
   return (
-    <View>
-      <Text>NavigateCard</Text>
+    <View className='bg-white flex-1'>
+      <Text className='text-center py-5 text-xl'>NavigateCard</Text>
+      <View className='border-t border-gray-200 flex-shrink'>
+        <View>
+            <GooglePlacesAutocomplete placeholder=''/>
+        </View>
+      </View>
     </View>
   )
 }
