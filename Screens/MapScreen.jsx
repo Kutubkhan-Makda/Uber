afe4a995de2ad59ea8cd1b7f20ba077a0fc6448b
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-13 17:04:04 +0530

    day-7

--- Screens/MapScreen.jsx
+++ Screens/MapScreen.jsx
@@ -1,16 +1,21 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Map from '../components/Map'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 const MapScreen = () => {
-  
+  const Stack = createNativeStackNavigator();
 
   return (
     <View>
       <View className='h-1/2'>
         <Map/>
       </View>
-      <View className='h-1/2'></View>
+      <View className='h-1/2'>
+        <Stack.Navigator>
+          
+        </Stack.Navigator>
+      </View>
     </View>
   )
 }
