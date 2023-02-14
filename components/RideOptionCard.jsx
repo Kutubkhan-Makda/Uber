0e84576decb2b337737563dd147c031720caebc9
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-14 19:45:07 +0530

    day-8

--- components/RideOptionCard.jsx
+++ components/RideOptionCard.jsx
@@ -1,12 +1,16 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import { Icon } from '@rneui/themed';
+import { useNavigation } from '@react-navigation/native';
 
 const RideOptionCard = () => {
+  const navigation = useNavigation();
+
   return (
     <View className='bg-white flex-grow'>
       <View>
-        <TouchableOpacity>
-          
+        <TouchableOpacity onPress={()=>navigation.navigate('NavigateCard')} className='absolute top-3 left-5 z-50 p-3 rounded-full'>
+          <Icon name='chevron-left' type='fontawsome'/>
         </TouchableOpacity>
         <Text className='text-center text-xl py-5'>Select A Ride</Text>
       </View>
