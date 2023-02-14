5f1e9d6cccb1af5b1876458f7139b4e9f4558bcc
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-14 19:32:39 +0530

    day-8

--- components/RideOptionCard.jsx
+++ components/RideOptionCard.jsx
@@ -1,10 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 
 const RideOptionCard = () => {
   return (
-    <View>
-      <Text className='text-center text-xl py-5'>Select A Ride</Text>
+    <View className='bg-white flex-grow'>
+      <View>
+        <TouchableOpacity>
+          
+        </TouchableOpacity>
+        <Text className='text-center text-xl py-5'>Select A Ride</Text>
+      </View>
     </View>
   )
 }
